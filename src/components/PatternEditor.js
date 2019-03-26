2699b59711b07a5198786b01443b31ccdc37e5f7
Tijmen Mulder <tmulder@cloudsuite.com>
2019-03-26 10:09:18 +0100

    style changes

--- src/components/PatternEditor.js
+++ src/components/PatternEditor.js
@@ -4,48 +4,46 @@ import TrackParameters from './TrackParameters'
 import ValueController from './ValueController'
 import TriggerController from './TriggerController'
 import AppContext from '../AppContext'
-import './PatternEditor.css'
+import styles from './PatternEditor.module.css'
 
 class PatternEditor extends Component {
   render () {
     return (
-      <div className="PatternEditor">
-        <div className="pattern-editor">
+      <div className={styles.PatternEditor}>
+        <AppContext.Consumer>
+          {state => (
+            <ParameterGroups
+              groups={state.groups}
+              setGroup={state.setGroup} />
+          )}
+        </AppContext.Consumer>
+        <div className={styles.sequencer}>
           <AppContext.Consumer>
             {state => (
-              <ParameterGroups
-                groups={state.groups}
-                setGroup={state.setGroup} />
+              <TrackParameters
+                parameters={state.parameters[state.group]}
+                setParameter={state.setParameter} />
             )}
           </AppContext.Consumer>
-          <div className="sequencer">
-            <AppContext.Consumer>
-              {state => (
-                <TrackParameters
-                  parameters={state.parameters[state.group]}
-                  setParameter={state.setParameter} />
-              )}
-            </AppContext.Consumer>
-            <AppContext.Consumer>
-              {state => (
-                <ValueController
-                  values={state.values}
-                  setValue={state.setValue} />
-              )}
-            </AppContext.Consumer>
-            <AppContext.Consumer>
-              {state => (
-                <TriggerController
-                  steps={state.steps}
-                  setStep={state.setStep} />
-              )}
-            </AppContext.Consumer>
-            <div className="subdivisions">
-              <div className="item"></div>
-              <div className="item"></div>
-              <div className="item"></div>
-              <div className="item"></div>
-            </div>
+          <AppContext.Consumer>
+            {state => (
+              <ValueController
+                values={state.values}
+                setValue={state.setValue} />
+            )}
+          </AppContext.Consumer>
+          <AppContext.Consumer>
+            {state => (
+              <TriggerController
+                steps={state.steps}
+                setStep={state.setStep} />
+            )}
+          </AppContext.Consumer>
+          <div className={styles.subdivision}>
+            <div className={styles.subdivisionItem}></div>
+            <div className={styles.subdivisionItem}></div>
+            <div className={styles.subdivisionItem}></div>
+            <div className={styles.subdivisionItem}></div>
           </div>
         </div>
       </div>
